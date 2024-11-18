aa09b56693733a8a9d5e9aceba4fae60901fd23f
Nelson Mbau <mbau.ndungu@gmail.com>
2024-11-18 12:27:38 +0300

    feat: add routes for visits

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,6 +3,7 @@ import { Hono } from 'hono'
 
 import { homes } from './homes'
 import { donationsRouter } from './donations'
+import { visitsRouter } from './visits'
 
 const app = new Hono().basePath('/api')
 
@@ -10,6 +11,7 @@ const setupRoutes = () => {
   app.get('/', c => c.text('Hono!'))
   app.route('/homes', homes)
   app.route('/donations', donationsRouter)
+  app.route('/visits', visitsRouter)
 }
 
 export { app, setupRoutes }
